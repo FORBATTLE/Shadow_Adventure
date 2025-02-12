1eb74bcfb61225168be5313626ca00ab40cee1a5
Alcides Josue Blanco Flores <ab213794@ebrschools.org>
2025-02-12 08:43:15 -0600

    Added a bunch of code for the damaged state, death state, and a health system. This also includes all the important animations for the character.

--- shadow_adventure/Player/Samurai_Archer/archer.gd
+++ shadow_adventure/Player/Samurai_Archer/archer.gd
@@ -3,13 +3,21 @@ extends CharacterBody2D
 
 var SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+###
 @onready var AP: AnimationPlayer = $AnimationPlayer
 var facing_direction = 1  # 1 = Right, -1 = Left
+var is_moving = false
+var is_shooting = false
+###
+@export var max_health: int = 100
+var current_health: int
 
+
+##
 @export var arrow = preload("res://Player/Samurai_Archer/arrow.tscn")
 var can_shoot = true
 var shoot_timer 
-
+##
 func _ready():
 	# Create and configure the timer
 	shoot_timer = Timer.new()
@@ -17,14 +25,18 @@ func _ready():
 	shoot_timer.one_shot = true
 	shoot_timer.timeout.connect(_reset_shoot)
 	add_child(shoot_timer)
-
+	current_health = max_health
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	
+	
 	var move_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
 	if move_input != 0:
 		facing_direction = 1 if move_input > 0 else -1
-
+		is_moving = true
+	else:
+		is_moving = false
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -32,33 +44,36 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		AP.play("Jump")
 	
 
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction>0:
-		$"Regular Archer".flip_h = false
-	elif direction<0:
-		$"Regular Archer".flip_h = true
-		
-	
+	$"Regular Archer".flip_h = (facing_direction == -1)
 	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if Input.is_action_just_pressed("ui_right") and is_on_floor():
-		AP.play("Run")
-		
-	elif Input.is_action_just_pressed("ui_right") and is_on_floor():
-		AP.play("LRun")
+	
+	
+	
+	if not is_on_floor():
+		$AnimationPlayer.play("Jump")
+	
+	elif is_moving and not is_shooting:
+		$AnimationPlayer.play("Run")
+	elif not is_moving and not is_shooting:
+		$AnimationPlayer.play("Idle")
 	if Input.is_action_just_pressed("ui_accept") and can_shoot:
 		fire_projectile()
+		play_shoot_animation()
+	
 	
 	move_and_slide()
-#func shoot():
+
 	#if Input.is_action_just_pressed("ui_accept"):
 		#var new_arrow = arrow.instantiate()
 		#var arrow_vec = $bow.global_position - $ArcPlayer.global_position
@@ -74,8 +89,24 @@ func fire_projectile():
 	new_arrow.position = $bow.global_position + spawn_offset  # Spawn at a specified node (GunPoint)
 	new_arrow.set_direction(facing_direction)  # Call the function to set direction
 	get_parent().add_child(new_arrow)
-	
+	AP.play("Attack")
 func _reset_shoot():
 	can_shoot = true  # Allow shooting again
+func play_shoot_animation():
+	is_shooting = true
+	$AnimationPlayer.play("Attack")  # Play "shoot_right" and flip sprite if needed
 
-	
+	# Set a timer to reset shooting state after 0.5 seconds
+	await get_tree().create_timer(0.5).timeout
+	is_shooting = false
+func take_damage(amount: int):
+	# Reduce health by the damage amount
+	current_health -= amount
+	current_health = clamp(current_health, 0, max_health)  # Ensure it doesn’t go below 0
+	if current_health <= 0:
+		die()
+func die():
+	# Handle player death (e.g., play animation, reset level)
+	print("Player has died!")
+	# Optionally, restart the scene or disable player controls
+	get_tree().reload_current_scene()
